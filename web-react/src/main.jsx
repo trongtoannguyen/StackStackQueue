f8828063089e97d6ffe2e2ba2f958da83c85e946
Vincent142857 <vinhlong505@gmail.com>
2024-04-23 08:38:03 +0700

    fix ui,socialoauth

--- web-react/src/main.jsx
+++ web-react/src/main.jsx
@@ -13,9 +13,6 @@ import { Provider } from 'react-redux';
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
-// import { GoogleOAuthProvider } from '@react-oauth/google';
-// const clientId = "951311377160-rqh5urnkrq2tps6ldc6gm7uhnhosp2lh.apps.googleusercontent.com"
-// const clientSecret = "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";
 
 import App from './App.jsx'
 import './index.scss'
@@ -23,11 +20,11 @@ import './index.scss'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-        <React.StrictMode>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </React.StrictMode>
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
     </PersistGate>
   </Provider>
 )
